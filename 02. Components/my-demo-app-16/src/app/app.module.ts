ffa09b4015224b4fea26cc483e6cdad576ea925d
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-03-09 22:29:55 +0200

    components

---
+++ 02. Components/my-demo-app-16/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.components';
+import { NavigationComponent } from './navigation/navigation.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    NavigationComponent,
+    UserListComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+// class Box {
+//   color: string = 'white';
+
+//   constructor(color: string) {
+//     this.color = color;
+//   }
+// }
+
+// const color = ['green', 'red', 'blue', 'brown'];
+
+// color.forEach(color => {
+//   const box = new Box(color);
+// })
