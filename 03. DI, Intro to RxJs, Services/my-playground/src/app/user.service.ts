6ded11c7186ff53ec68df4ae083d9521d3b9aa93
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-03-11 12:23:24 +0200

    promise

--- 03. DI, Intro to RxJs, Services/my-playground/src/app/user.service.ts
+++ 03. DI, Intro to RxJs, Services/my-playground/src/app/user.service.ts
@@ -26,3 +26,31 @@ export class UserService implements OnDestroy {
     inputAge.value = ''
   }
 }
+
+//PROMISE
+// - promises are not lazy, code invokes
+// - can not be canceled
+const p = new Promise((resolve, reject) => {
+  // console.log('promise involked');
+
+  setTimeout(() => {
+    resolve(111)
+    // reject('Why not?')
+  }, 30000);
+});
+
+p.then((data) => {
+  console.log('from propmise', data);
+}).then().then().catch(err => console.log('Error:', err));
+
+//way 2
+Promise.resolve(112).then(data => data * 10).then(x => console.log('from promise 2:', x));
+
+//sync analogy
+[1].map((x) => x * 2);
+
+//Obserbables
+//sync analogy
+[1,2,3,4].map((x) => x * 2);
+
+
