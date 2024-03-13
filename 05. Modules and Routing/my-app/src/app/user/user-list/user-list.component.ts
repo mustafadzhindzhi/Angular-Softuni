2f75c80f972cffb803a8e101d34fa6211df7f186
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-03-13 22:25:27 +0200

    modules

---
+++ 05. Modules and Routing/my-app/src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent {
+  isLoading = false;
+
+  fetchUsers () {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      this.isLoading = false
+    }, 3000);
+  }
+}
